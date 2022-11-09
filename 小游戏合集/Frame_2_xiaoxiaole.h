08f1b567eba2dc84ac772290eaa950157e02567d
704214015@qq.com <704214015@qq.com>
2022-11-09 23:59:03 +0800

    1. 增加消消乐玩法。   2. 下一步需要分析一个潜在的引发崩溃的问题点。   3. 下一步实现方块的重力掉落

---
+++ 小游戏合集/Frame_2_xiaoxiaole.h
@@ -0,0 +1,185 @@
+﻿#pragma once
+#include "Frame.h"
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+#define MAX_BLOCK_NUM 50
+#define MAX_COLOR_TYPE 7
+
+class Frame_2_xiaoxiaole : public Frame {
+private:
+	int mBoardX;
+	int mBoardY;
+	int mBoardWidth;
+	int mBoardHeight;
+	COLORREF mBoardBkColor;
+	COLORREF mBoardOutlineColor;
+	int mBoardData[MAX_BLOCK_NUM][MAX_BLOCK_NUM]; // -1代表没有颜色，数组下标的值表示对应颜色
+	int mBlockNum = 8;  // 规格设置为 blockNum * blockNum
+	int mColorTypeNum = 4;
+	COLORREF mColorTypes[MAX_COLOR_TYPE] = { RED, GREEN, BLUE, YELLOW, CYAN, BROWN, MAGENTA };
+
+public:
+	Frame_2_xiaoxiaole(int frameWidth, int frameHeight, COLORREF frameBkColor, int globalIndex,
+		int boardX, int boardY, int boardWidth, int boardHeight, COLORREF boardBkColor, COLORREF boardOutlineColor) :
+		Frame(frameWidth, frameHeight, frameBkColor, globalIndex) {
+		mBoardX = boardX;
+		mBoardY = boardY;
+		mBoardWidth = boardWidth;
+		mBoardHeight = boardHeight;
+		mBoardBkColor = boardBkColor;
+		mBoardOutlineColor = boardOutlineColor;
+	}
+
+	virtual void init() override {
+		BeginBatchDraw();
+		setbkcolor(mFrameBkColor);
+		cleardevice();
+		for (int i = 0; i < mButtonNum; i++) {
+			mButtonList[i]->draw();
+		}
+		for (int i = 0; i < mTextNum; i++) {
+			mTextList[i]->draw();
+		}
+
+		initBoard();
+
+		FlushBatchDraw();
+		EndBatchDraw();
+	}
+
+	void initBoardData() {
+		// 随机数种子是unsigned int型的，而time是秒级的，所以如果按钮一秒内被按下多次，这一秒内的随机数种子就会是一样的
+		// 进而导致一秒内按下多次但是界面不刷新。暂无解决方法
+		srand(time(NULL));
+		for (int i = 0; i < MAX_BLOCK_NUM; i++) {
+			for (int j = 0; j < MAX_BLOCK_NUM; j++) {
+				mBoardData[i][j] = rand() % mColorTypeNum;
+			}
+		}
+	}
+
+	void initBoard() {
+		initBoardData();
+		setfillcolor(mBoardBkColor);
+		solidrectangle(mBoardX, mBoardY, mBoardX + mBoardWidth, mBoardY + mBoardHeight);
+		drawBoardBlock();
+		drawBoardLine();
+	}
+
+	void drawBoardLine() const {
+		setlinestyle(PS_SOLID | PS_ENDCAP_FLAT, 1);
+		setlinecolor(mBoardOutlineColor);
+
+		int blockHeight = mBoardHeight / mBlockNum;
+		int blockWidth = mBoardWidth / mBlockNum;
+		for (int i = 0; i <= mBlockNum; i++) {
+			line(mBoardX, mBoardY + i * blockHeight, mBoardX + mBoardWidth, mBoardY + i * blockHeight);
+		}
+		for (int i = 0; i <= mBlockNum; i++) {
+			line(mBoardX + i * blockWidth, mBoardY, mBoardX + i * blockWidth, mBoardY + mBoardHeight);
+		}
+	}
+
+	void drawBoardBlock() const {
+		int blockHeight = mBoardHeight / mBlockNum;
+		int blockWidth = mBoardWidth / mBlockNum;
+		for (int i = 0; i < mBlockNum; i++) {
+			for (int j = 0; j < mBlockNum; j++) {
+				int currColorType = mBoardData[i][j];
+				if (currColorType == -1) {
+					setfillcolor(mBoardBkColor);
+				} else {
+					setfillcolor(mColorTypes[currColorType]);
+				}
+				solidrectangle(mBoardX + i * blockWidth, mBoardY + j * blockHeight, mBoardX + (i + 1) * blockWidth, mBoardY + (j + 1) * blockHeight);
+			}
+		}
+	}
+
+	virtual void processEvent(int eventIndex, int mouseX, int mouseY) override {
+		BeginBatchDraw();
+		if (eventIndex == -1) {
+			processBoardClick(mouseX, mouseY);
+		} else if (eventIndex == 8) {
+			initBoard();
+		}
+		FlushBatchDraw();
+		EndBatchDraw();
+	}
+
+	void processBoardClick(int mouseX, int mouseY) {
+		if (mouseX <= mBoardX || mouseX >= mBoardX + mBoardWidth) {
+			return;
+		}
+		if (mouseY <= mBoardY || mouseY >= mBoardY + mBoardHeight) {
+			return;
+		}
+		int blockHeight = mBoardHeight / mBlockNum;
+		int blockWidth = mBoardWidth / mBlockNum;
+		int xIndex = (mouseX - mBoardX) / blockWidth;
+		int yIndex = (mouseY - mBoardY) / blockHeight;
+		int currColorType = mBoardData[xIndex][yIndex];
+		if (currColorType == -1) {
+			return;
+		}
+		clearSameColor(xIndex, yIndex);
+		letBlockDrop();
+		drawBoardBlock();
+		drawBoardLine();
+	}
+
+	int clearSameColor(int targetXIndex, int targetYIndex) {
+		int clearedBlockCount = 0;
+		int currColorType = mBoardData[targetXIndex][targetYIndex];
+		if (currColorType == -1) {
+			return 0;
+		}
+		std::vector<std::pair<int, int>> workList;
+		workList.push_back(std::make_pair(targetXIndex, targetYIndex));
+		while (workList.size() != 0) {
+			// 清除当前位置的颜色
+			auto it = workList.begin();
+			if (mBoardData[it->first][it->second] == currColorType) {
+				mBoardData[it->first][it->second] = -1;
+				clearedBlockCount++;
+			}
+			// 观察上方是否同色
+			it = workList.begin();
+			if (it->second > 0 && mBoardData[it->first][it->second - 1] == currColorType) {
+				workList.push_back(std::make_pair(it->first, it->second - 1));
+				//mBoardData[it->first][it->second - 1] = -1;  // 取消此处注释后为什么会在一竖列同色情况下引发崩溃？
+				clearedBlockCount++;
+			}
+			// 观察下方是否同色
+			it = workList.begin();
+			if (it->second < mBlockNum - 1 && mBoardData[it->first][it->second + 1] == currColorType) {
+				workList.push_back(std::make_pair(it->first, it->second + 1));
+				//mBoardData[it->first][it->second + 1] = -1;
+				clearedBlockCount++;
+			}
+			// 观察左方是否同色
+			it = workList.begin();
+			if (it->first > 0 && mBoardData[it->first - 1][it->second] == currColorType) {
+				workList.push_back(std::make_pair(it->first - 1, it->second));
+				//mBoardData[it->first - 1][it->second] = -1;
+				clearedBlockCount++;
+			}
+			// 观察右方是否同色
+			it = workList.begin();
+			if (it->first < mBlockNum - 1 && mBoardData[it->first + 1][it->second] == currColorType) {
+				workList.push_back(std::make_pair(it->first + 1, it->second));
+				//mBoardData[it->first + 1][it->second] = -1;
+				clearedBlockCount++;
+			}
+
+			workList.erase(workList.begin());
+		}
+		return clearedBlockCount;
+	}
+
+	void letBlockDrop() {
+		//
+	}
+};
--- 小游戏合集/main.cpp
+++ 小游戏合集/main.cpp
@@ -5,6 +5,7 @@
 #include "Frame.h"
 #include "Frame_0_menu.h"
 #include "Frame_1_jingziqi.h"
+#include "Frame_2_xiaoxiaole.h"
 #include "TextLabel.h"
 
 int main() {
@@ -19,6 +20,8 @@ int main() {
 	Frame_0_menu frame_0_menu(bkWidth, bkHeight, bkColor, 0);
 	Frame_1_jingziqi frame_1_jingziqi(bkWidth, bkHeight, bkColor, 1,
 		(bkWidth - bkHeight / 2) / 2, bkHeight / 4, bkHeight / 2, bkHeight / 2, bkColor, BLACK);
+	Frame_2_xiaoxiaole frame_2_xiaoxiaole(bkWidth, bkHeight, bkColor, 2,
+		200, 180, 600, 600, bkColor, BLACK);
 
 	// 创建Button
 	Button button_0_jingziqi(100, 100, 130, 50, (char*)"井字棋", bkColor, 0);
@@ -27,6 +30,9 @@ int main() {
 	Button button_3_frame_1_loadPlayer1Pic(370, 10, 190, 50, (char*)"玩家1图片", bkColor, 3);
 	Button button_4_frame_1_loadPlayer2Pic(570, 10, 190, 50, (char*)"玩家2图片", bkColor, 4);
 	Button button_5_frame_1_ResetDefaultPic(770, 10, 170, 50, (char*)"重置图片", bkColor, 5);
+	Button button_6_xiaoxiaole(100, 160, 130, 50, (char*)"消消乐", bkColor, 6);
+	Button button_7_frame_2_to_frame_0(10, 10, 170, 50, (char*)"回到菜单", bkColor, 7);
+	Button button_8_frame_2_playAgain(190, 10, 170, 50, (char*)"重玩", bkColor, 8);
 
 	// 注册Button, 顺序要严格按照Button index添加
 	Button* allButtons[100];
@@ -36,6 +42,9 @@ int main() {
 	allButtons[3] = &button_3_frame_1_loadPlayer1Pic;
 	allButtons[4] = &button_4_frame_1_loadPlayer2Pic;
 	allButtons[5] = &button_5_frame_1_ResetDefaultPic;
+	allButtons[6] = &button_6_xiaoxiaole;
+	allButtons[7] = &button_7_frame_2_to_frame_0;
+	allButtons[8] = &button_8_frame_2_playAgain;
 
 	int frame1TextX = (bkWidth - bkHeight / 2) / 2;
 	int frame1TextY = bkHeight / 4 * 3;
@@ -49,14 +58,20 @@ int main() {
 	TextLabel text_5_frame_1(frame1TextX, frame1TextY + frame1TextHeight, frame1TextWidth, frame1TextHeight * 0.8, bkColor, BLACK, (char*)"玩家1赢 : 平局 : 玩家2赢", true, 5);
 	TextLabel text_6_frame_1(frame1TextX, frame1TextY + frame1TextHeight * 1.6, frame1TextWidth, frame1TextHeight * 1.6, bkColor, BLACK, (char*)"0:0:0", true, 6);
 	TextLabel text_7_frame_1(frame1TextX, bkHeight / 4 - frame1TextHeight * 2, frame1TextWidth, frame1TextHeight * 2, bkColor, BLACK, (char*)"井字棋", true, 7);
+	TextLabel text_8_frame_2(350, 70, 300, 100, bkColor, BLACK, (char*)"消消乐", true, 8);
 
 	// 将Button加入所属的Frame
 	frame_0_menu.addButton(&button_0_jingziqi);
+	frame_0_menu.addButton(&button_6_xiaoxiaole);
+
 	frame_1_jingziqi.addButton(&button_1_frame_1_to_frame_0);
 	frame_1_jingziqi.addButton(&button_2_frame_1_playAgain);
 	frame_1_jingziqi.addButton(&button_3_frame_1_loadPlayer1Pic);
 	frame_1_jingziqi.addButton(&button_4_frame_1_loadPlayer2Pic);
 	frame_1_jingziqi.addButton(&button_5_frame_1_ResetDefaultPic);
+
+	frame_2_xiaoxiaole.addButton(&button_7_frame_2_to_frame_0);
+	frame_2_xiaoxiaole.addButton(&button_8_frame_2_playAgain);
 
 	frame_1_jingziqi.addText(&text_0_frame_1);
 	frame_1_jingziqi.addText(&text_1_frame_1);
@@ -66,6 +81,9 @@ int main() {
 	frame_1_jingziqi.addText(&text_5_frame_1);
 	frame_1_jingziqi.addText(&text_6_frame_1);
 	frame_1_jingziqi.addText(&text_7_frame_1);
+
+	frame_2_xiaoxiaole.addText(&text_8_frame_2);
+	
 
 	// 初始化第一个窗口
 	currFrame = &frame_0_menu;
@@ -91,6 +109,12 @@ int main() {
 				currFrame = &frame_1_jingziqi;
 				currFrame->init();
 			} else if (currClickedButtonIndex == 1) {
+				currFrame = &frame_0_menu;
+				currFrame->init();
+			} else if (currClickedButtonIndex == 6) {
+				currFrame = &frame_2_xiaoxiaole;
+				currFrame->init();
+			} else if (currClickedButtonIndex == 7) {
 				currFrame = &frame_0_menu;
 				currFrame->init();
 			}
