e09faf9b44d723cfc082b468a39d44d049d0a680
704214015@qq.com <704214015@qq.com>
2023-01-07 21:12:50 +0800

    1. 新增TimeCount模块用于计时    2. 新增素材图片

--- 小游戏合集/Frame_5_lianliankan.h
+++ 小游戏合集/Frame_5_lianliankan.h
@@ -1,13 +1,16 @@
 ﻿#pragma once
 #include "Frame.h"
+#include "TimeCount.h"
 
 class Frame_5_lianliankan : public Frame {
 private:
-
+	CountDown mCountDown;
+	
 
 public:
 	Frame_5_lianliankan(int frameWidth, int frameHeight, COLORREF frameBkColor, int globalIndex) :
-		Frame(frameWidth, frameHeight, frameBkColor, globalIndex) {
+		Frame(frameWidth, frameHeight, frameBkColor, globalIndex),
+		mCountDown(100, 100, 200, 80, frameBkColor, BLACK, 20) {
 	}
 
 	virtual void init() override {
@@ -21,11 +24,17 @@ public:
 		}
 	}
 
+	
+
 	virtual void processEvent(int eventIndex, int mouseX, int mouseY) override {
 		if (eventIndex == 36) {
-
+			mCountDown.startCounting();
 		} else if (eventIndex == 37) {
 
 		}
+	}
+
+	virtual void processSomethingInLoop() override {
+		mCountDown.doCounting();
 	}
 };
---
+++ 小游戏合集/TimeCount.h
@@ -0,0 +1,92 @@
+﻿#pragma once
+#include <time.h>
+#include <graphics.h>
+#include <stdio.h>
+
+class CountDown {
+private:
+	int mX;
+	int mY;
+	int mWidth;
+	int mHeight;
+	COLORREF mBkColor;
+	COLORREF mFontColor;
+	bool mEnable = false;
+	bool mIsCounting = false;   // 倒计时是否结束
+	unsigned mAllSeconds = 0;   // 倒计时开始时的初始秒
+	unsigned mCurrSeconds = 0;
+	time_t mCountStart = { 0 };
+
+public:
+	CountDown(int x, int y, int width, int height, COLORREF bkColor, COLORREF fontColor, unsigned allSeconds) {
+		mX = x;
+		mY = y;
+		mWidth = width;
+		mHeight = height;
+		mAllSeconds = allSeconds;
+		mBkColor = bkColor;
+		mFontColor = fontColor;
+	}
+
+	void startCounting() {
+		mEnable = true;
+		mIsCounting = true;
+		mCurrSeconds = mAllSeconds;
+		mCountStart = time(nullptr);
+		draw();
+	}
+
+	void stopCounting() {
+
+	}
+
+	void setEnable(bool enable) {
+		mEnable = enable;
+	}
+
+	void doCounting() {
+		if (mEnable == false || mIsCounting == false) {
+			return;
+		}
+		time_t currTime = time(nullptr);
+		time_t interval = currTime - mCountStart;
+		if (interval > mAllSeconds) {
+			mCurrSeconds = 0;
+			return;
+		}
+		if (mCurrSeconds == mAllSeconds - interval) { // 一秒以内，不需要更新，防止draw调用次数过多
+			return;
+		}
+		mCurrSeconds = mAllSeconds - interval;
+		draw();
+	}
+
+	void clear() const {
+		if (mEnable == false || mIsCounting == false) {
+			return;
+		}
+		setfillcolor(mBkColor);
+		solidrectangle(mX, mY, mX + mWidth, mY + mHeight);
+	}
+
+	void draw() const {
+		if (mEnable == false || mIsCounting == false) {
+			return;
+		}
+		clear();
+		int minutes = mCurrSeconds / 60;
+		int seconds = mCurrSeconds - minutes * 60;
+		setlinecolor(mFontColor);
+		setlinestyle(PS_SOLID | PS_ENDCAP_FLAT, 1);
+		rectangle(mX, mY, mX + mWidth, mY + mHeight);
+		char temp[20] = { 0 };
+		snprintf(temp, 20 - 1, "%d:%.2d", minutes, seconds);
+		int textLen = strlen(temp);
+		int fontHeight = mHeight * 0.8;
+		int fontWidth = mWidth / textLen;
+		settextstyle(fontHeight, fontWidth, _T("Consolas"));
+		setbkmode(TRANSPARENT);
+		settextcolor(mFontColor);
+		outtextxy(mX, mY + mHeight * 0.1, temp);
+	}
+};
